8cf182ab8e8e1b340d992d06e84c2df0c0c53987
innisa <innisa@bishopireton.org>
2017-03-09 13:13:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/innisa/magiceightball3_9/scrnGame.java
@@ -0,0 +1,13 @@
+package com.example.innisa.magiceightball3_9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scrnGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrn_game);
+    }
+}
